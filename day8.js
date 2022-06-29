96f31942c91f9adb6f6ff9e8936458417dcd3b29
viveksinghkushwah <viveksingh241199@gmail.com>
2022-06-29 18:46:29 +0530

    q

--- day8.js
+++ day8.js
@@ -10,6 +10,12 @@ function evennum(x){
     return console.log(text);
 }
 evennum(17);
+
+//output
+/*0 2 4 6 8 10 12 14 16 */ 
+
+
+//write a function to take two input of empoyee and  a number and display all the employee less than that age
 y=[]
  function agefilter(a,b){
     y= a.filter(a => a.age>b);
@@ -20,13 +26,20 @@ y=[]
  x=[{name:"vivek",age:22},{name:"aa",age:12},{name:"bb",age:7},{name:"ccc",age:14},];
  agefilter(x,10)
  console.log(y)
+ /*
+ output[
+  { name: 'vivek', age: 22 },
+  { name: 'aa', age: 12 },
+  { name: 'ccc', age: 14 }
+] */
 
+ //create a function which take two input fist the array of emplyee data second the attribute for sorting
 
  let salary;
 employeedata=[{name:"vivek",age:22,salary:20000},
                 {name:"aa",age:12,salary:12500},
                 {name:"bb",age:7,salary:45058},
-                {name:"ccc",age:14,salary:25000},];
+                {name:"ccc",age:14,salary:25000}+,];
  function sortdata(w,e){
 if (e=="age"){
     let neww=w.sort(function(a,b){return a.age - b.age})   ;
@@ -54,3 +67,11 @@ else
  q="salary";
  sortdata(employeedata,q)
  console.log(neww);
+ /*output
+ [
+  { name: 'aa', age: 12, salary: 12500 },
+  { name: 'vivek', age: 22, salary: 20000 },
+  { name: 'ccc', age: 14, salary: 25000 },
+  { name: 'bb', age: 7, salary: 45058 }
+]
+ */
